2a40cbb2ff6f26005ba47725fbd79883e743ccb8
Hitesh S V <hitesh@ghoshak.com>
2022-04-17 21:59:06 +0530

    sass update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import img from "../src/Images/logo1.png";
 
 function App() {
   return (
     <>
       <header className="header">
         <div className="logo-box">
-          <img src="../../Images/logo1.png" alt="tigerlogo" className="logo" />
+          <img src={img} alt="tigerlogo" className="logo" />
         </div>
         <div className="heading">
           <h1 className="heading-primary">
             <span className="heading-primary-main">Outdoors</span>
-            <span className="heading-primary-sub">is where life happens...</span>
+            <span className="heading-primary-sub">
+              is where life happens...
+            </span>
           </h1>
           <a href="#" className="btn btn-white btn-animated">
             Discover our tours
