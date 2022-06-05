95247406ee32062a7b9b2c7fbd0ba7924d12cb8d
Hitesh S V <hitesh@ghoshak.com>
2022-06-05 23:56:29 +0530

    booking section completed

--- src/App.js
+++ src/App.js
@@ -328,6 +328,76 @@ function App() {
             </a>
           </div>
         </section>
+
+        {/* Fifth section */}
+        <section className="section-book">
+          <div className="row">
+            <div className="book">
+              <div className="book__form">
+                <form action="#" className="from">
+                  <div className="u-margin-bottom-medium">
+                    <h2 className="heading-secondary">Start Booking now</h2>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      className="form__input"
+                      placeholder="Full Name"
+                      id="name"
+                      required
+                    />
+                    <label for="" className="form__label">
+                      Full Name
+                    </label>
+                  </div>
+
+                  <div className="form__group">
+                    <input
+                      type="email"
+                      className="form__input"
+                      placeholder="Email address"
+                      id="email"
+                      required
+                    />
+                    <label for="" className="form__label">
+                      Email address
+                    </label>
+                  </div>
+
+                  <div className="form__group u-margin-bottom-medium">
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="small"
+                        name="size"
+                      />
+                      <label for="small" className="form__radio-label">
+                        <span className="form__radio-button"></span>
+                        Small tour group
+                      </label>
+                    </div>
+                    <div className="form__radio-group">
+                      <input
+                        type="radio"
+                        className="form__radio-input"
+                        id="large"
+                        name="size"
+                      />
+                      <label for="large" className="form__radio-label">
+                        <span className="form__radio-button"></span>
+                        Large tour group
+                      </label>
+                    </div>
+                  </div>
+                  <div className="form__group">
+                    <button className="btn btn--green">Next step &rarr;</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
 
       {/* <section className="grid-test">
